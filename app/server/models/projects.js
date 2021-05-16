23584ca54a867915083c968318a611edc93bb493
unknown <ore.craftman@gmail.com>
2021-05-16 19:57:03 +0100

    Added initial JavaScript files to the Project

--- app/server/models/projects.js
+++ app/server/models/projects.js
@@ -15,30 +15,19 @@ class Projects extends DataModel {
     validate(obj) {
         // this.errors.splice(0, this.errors.length)
         this.errors = []
-        if (!obj.hasOwnProperty("id") || obj.id === '' || obj.id === undefined || obj.id === null) {
-            this.errors.push("id should not be empty")
-        }
-        if (!obj.hasOwnProperty("name") || obj.name === '' || obj.name === undefined || obj.name === null) {
-            this.errors.push("name should not be empty")
-        }
-        if (!obj.hasOwnProperty("abstract") || obj.abstract === '' || obj.abstract === undefined || obj.abstract === null) {
-            this.errors.push("abstract should not be empty")
-        }
-        if (!obj.hasOwnProperty("authors") || obj.abstract === '' || obj.abstract === undefined || obj.abstract === null) {
-            this.errors.push("authors should not be empty")
-        }
-        if (!obj.hasOwnProperty("tags") || obj.tags === '' || obj.tags === undefined || obj.tags === null) {
-            this.errors.push("tags should not be empty")
-        }
-        if (!obj.hasOwnProperty("createdBy") || obj.createdBy === '' || obj.createdBy === undefined || obj.createdBy === null) {
-            this.errors.push("createdBy should not be empty")
-        }
-
         if (!Array.isArray(obj.authors)) {
             this.errors.push("Authors should be an array");
         }
         if (!Array.isArray(obj.tags)) {
             this.errors.push("Tags should be an array");
+        }
+
+        for (const key in obj) {
+            if (obj[key] === '' || obj[key] === undefined || obj[key] === null) {
+                if (key !== 'authors' && key !== 'tags') {
+                    this.errors.push(`${key} should not be empty.`)
+                }
+            }
         }
 
         if (this.errors.length > 0) {
